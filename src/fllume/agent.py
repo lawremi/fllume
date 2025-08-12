94eb7d276de81822275065220719a4440b84ad3f
Michael Lawrence <lawremi@gmail.com>
2025-08-11 18:24:11 -0700

    sketch out tools and tool call handling

--- src/fllume/agent.py
+++ src/fllume/agent.py
@@ -1,5 +1,5 @@
 import any_llm
-from typing import Type, Iterator, Union, Generator, Any
+from typing import Type, Iterator, Union, Generator, Any, Callable
 
 class Agent:
     def __init__(
@@ -7,13 +7,15 @@ class Agent:
         model: str, 
         context: list[dict[str, Any]], 
         instructions: str, 
+        tools: list[Callable[..., Any]],
         params: dict[str, Any]
     ):
         self.model = model
         self.context = context
         self.instructions = instructions
+        self.tools = tools
         self.params = params
-    
+        
     @classmethod
     def builder(cls) -> 'AgentBuilder':
         return AgentBuilder(cls)
@@ -30,33 +32,65 @@ class Agent:
             if content is not None:
                 yield content
 
-    def complete(
-        self, prompt: str, stream: bool = False
-    ) -> Union[str, Generator[str, None, None]]:
+    def complete_with_context(
+        self, 
+        context: list[dict[str, Any]] = [],
+        stream: bool = False
+    ):
         completion = any_llm.completion(
             self.model, 
-            messages = (
-                self.context
-                + (
-                    [{"role": "system", "content": self.instructions}] 
-                    if self.instructions else []
-                )
-                + [{"role": "user", "content": prompt}]
-            ),
+            messages = context,
             stream = stream,
+            tools = self.tools
             **self.params
         )
         if stream:
             return self._stream_completion(completion)
         else:
-            return completion.choices[0].message.content
+            return context + completion.choices[0].message
+
+    def _call_tools(self, tool_calls: dict[str, Any]):
+        tool_dict = {tool.__name__: tool for tool in self.tools}
+        tool_messages = [
+            {
+                "role": "tool",
+                "tool_call_id": tool_call.id,
+                "content": tool_dict[tool_call.function.name](
+                    **tool_call.function.arguments
+                )
+            }
+            for tool_call in tool_calls
+        ]
+        return tool_messages
     
+    def complete(
+        self, prompt: str, stream: bool = False
+    ) -> Union[str, Generator[str, None, None]]:
+        context = (
+            self.context
+            + (
+                [{"role": "system", "content": self.instructions}] 
+                if self.instructions else []
+            )
+            + [{"role": "user", "content": prompt}]
+        )
+        completion = self.complete_with_context(context, stream)
+        if stream:
+            return completion
+        else:
+            while len(completion[-1].tool_calls) > 0:
+                tool_messages = self._call_tools(completion[-1].tool_calls)
+                completion = self.complete_with_context(completion + tool_messages)
+            return completion[-1].content
+            
     def __repr__(self) -> str:
         return (
             f"Agent("
             f"model={self.model}, "
             f"context={self.context}, "
-            f"instructions={self.instructions}"
+            f"instructions={self.instructions}, "
+            f"tools={self.tools}, "
+            f"params={self.params}"
             f")"
         )
 
@@ -67,13 +101,15 @@ class AgentBuilder:
         self.context = []
         self.instructions = None
         self.params = {}
-
+        self.tools = []
+        
     def build(self) -> Agent:
         assert self.model is not None, "Model must be specified"
         return self.agent_cls(
             self.model, 
             self.context, 
             self.instructions,
+            self.tools,
             self.params
         )
 
@@ -89,6 +125,10 @@ class AgentBuilder:
         self.instructions = instructions
         return self
     
+    def with_tools(self, tools: list[Callable[..., Any]]):
+        self.tools = tools
+        return self
+
     def with_params(self, params: dict[str, Any]):
         self.params = params
         return self
--- tests/test_builder.py
+++ tests/test_builder.py
@@ -1,5 +1,4 @@
 import sys
-print(f"sys.path inside test: {sys.path}")
 import fllume
 
 def test_agent_builder_creates_agent_with_correct_model():
@@ -38,3 +37,19 @@ def test_agent_builder_creates_agent_with_correct_params():
     )
     assert isinstance(agent, fllume.Agent)
     assert agent.params == params
+
+def to_uppercase(text: str) -> str:
+    """Converts input text to uppercase."""
+    return text.upper()
+
+def test_agent_builder_creates_agent_with_correct_tools():
+    model_id = "test_provider/test_model"
+    tools = [to_uppercase]
+    agent = (
+        fllume.Agent.builder().
+        with_model(model_id).
+        with_tools(tools).
+        build()
+    )
+    assert isinstance(agent, fllume.Agent)
+    assert agent.tools == tools
